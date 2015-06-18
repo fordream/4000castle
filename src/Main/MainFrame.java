4f5302c4433e17810fdd29c4f3dd95e2cf08c3b6
DaehunGwak <pmon2648@daum.net>
2015-06-19 02:39:05 +0900

    BGM

--- src/Main/MainFrame.java
+++ src/Main/MainFrame.java
@@ -1,11 +1,17 @@
 package Main;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 
 import Controller.FileController;
 import Data.Game;
 import Edit.GUI.MapEditPanel;
 import Play.GUI.StagePanel;
+
 
 
 public class MainFrame extends SimpleJFrame 
@@ -40,6 +46,17 @@ public class MainFrame extends SimpleJFrame
 		this.add(mainPanel);
 		this.repaint();
 		
+		try{
+			File f = new File("audio/clozee - koto.wav");
+		    AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(f);
+		    
+		    Clip clip = AudioSystem.getClip();
+		    clip.open(audioInputStream);
+		    clip.start();
+		}
+		catch(Exception ex)
+		{
+		}
 		
 		
 		// TODO Auto-generated constructor stub
